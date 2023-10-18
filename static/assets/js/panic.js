aa41599bb2dab7086b2f92a6eb3fe18f080180c4
The Unblocked Hub <132709898+TheUnblockedHubOfficial@users.noreply.github.com>
2023-10-18 13:37:49 -0400

    Update panic.js

--- static/assets/js/panic.js
+++ static/assets/js/panic.js
@@ -1,34 +1,42 @@
-// Retrieve the event key and panic link from local storage or use defaults
-var eventKey = localStorage.getItem("eventKey") || "`";
-var panicLink = localStorage.getItem("panicLink") || "https://classroom.google.com/";
+import express from "express";
+import path from "path";
+import * as dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 
-// Function to handle keydown event
-document.addEventListener("keydown", function(event) {
-  if (event.key === eventKey) {
-    if (window.self !== window.top) {
-      window.parent.location.href = panicLink;
-    } else {
-      window.location.href = panicLink;
-    }
-  }
+dotenv.config();
+
+const __dirname = process.cwd();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define the rate limit settings
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
 });
 
-// Get eventKeyInput and linkInput elements by their IDs
-var eventKeyInput = document.getElementById("eventKeyInput");
-var linkInput = document.getElementById("linkInput");
+app.use(limiter); // Apply rate limiting to all routes
 
-// Add event listeners to update eventKey and panicLink when input changes
-eventKeyInput.addEventListener("input", function() {
-  eventKey = eventKeyInput.value;
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "static"));
+
+// Define your routes after the rate limiter
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "static", "index.html"));
 });
 
-linkInput.addEventListener("input", function() {
-  panicLink = linkInput.value;
+// Define other routes...
+
+app.get("/*", (req, res) => {
+  res.redirect("/404");
 });
 
-// Function to save eventKey and panicLink to local storage
-function saveEventKey() {
-  eventKey = eventKeyInput.value;
-  localStorage.setItem("eventKey", eventKey);
-  localStorage.setItem("panicLink", panicLink);
-}
+app.listen(port, () => {
+  console.log(`The Unblocked Hub running at http://localhost:${port}`);
+});
