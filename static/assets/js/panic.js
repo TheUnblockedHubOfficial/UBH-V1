fd53e9af319533b264aa838c60fd548823dbbec7
The Unblocked Hub <132709898+TheUnblockedHubOfficial@users.noreply.github.com>
2023-10-18 13:34:14 -0400

    Update panic.js

--- static/assets/js/panic.js
+++ static/assets/js/panic.js
@@ -1,6 +1,8 @@
+// Retrieve the event key and panic link from local storage or use defaults
 var eventKey = localStorage.getItem("eventKey") || "`";
 var panicLink = localStorage.getItem("panicLink") || "https://classroom.google.com/";
 
+// Function to handle keydown event
 document.addEventListener("keydown", function(event) {
   if (event.key === eventKey) {
     if (window.self !== window.top) {
@@ -11,16 +13,20 @@ document.addEventListener("keydown", function(event) {
   }
 });
 
+// Get eventKeyInput and linkInput elements by their IDs
 var eventKeyInput = document.getElementById("eventKeyInput");
+var linkInput = document.getElementById("linkInput");
+
+// Add event listeners to update eventKey and panicLink when input changes
 eventKeyInput.addEventListener("input", function() {
   eventKey = eventKeyInput.value;
 });
 
-var linkInput = document.getElementById("linkInput");
 linkInput.addEventListener("input", function() {
   panicLink = linkInput.value;
 });
 
+// Function to save eventKey and panicLink to local storage
 function saveEventKey() {
   eventKey = eventKeyInput.value;
   localStorage.setItem("eventKey", eventKey);
